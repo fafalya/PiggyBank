c4e8bc1651991f19505b8ab0343473737b9635a4
fafalya <jsankva@gmail.com>
2023-10-11 21:12:57 +0400

    create controller for AimEntity

--- PiggyBankBackEnd/Controllers/AimController.cs
+++ PiggyBankBackEnd/Controllers/AimController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PiggyBankBackEnd.Context;
 using PiggyBankBackEnd.DTOs;
+using PiggyBankBackEnd.Entities;
 
 namespace PiggyBankBackEnd.Controllers
 {
@@ -20,8 +22,85 @@ namespace PiggyBankBackEnd.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateAim([FromBody] CreateUpdateAimDTO dto)
         {
-            return null;
+            var newAim = new AimEntity()
+            {
+                Title = dto.Title,
+                Price = dto.Price,
+                Date = dto.Date,
+                PicturePath = dto.PicturePath,
+                WayOfSaving = dto.WayOfSaving
+
+            };
+            await _context.Aims.AddAsync(newAim);
+            await _context.SaveChangesAsync();
+
+            return Ok("New aim is successfully created");
         }
+
+        //read
+        [HttpGet]
+        public async Task<ActionResult<List<AimEntity>>> GetAllAims()
+        {
+            var aims = await _context.Aims.ToListAsync();
+
+            return Ok(aims);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<AimEntity>> GetAimById([FromRoute] long id)
+        {
+            var aim = await _context.Aims.FirstOrDefaultAsync(q => q.Id == id);
+
+            if(aim is null)
+            {
+                return NotFound("Aim is not found!");
+            }
+
+            return Ok(aim);
+        }
+
+        //update
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateAim([FromRoute] long id, [FromBody] CreateUpdateAimDTO dto)
+        {
+            var aim = await _context.Aims.FirstOrDefaultAsync(q => q.Id == id);
+
+            if (aim is null)
+            {
+                return NotFound("Aim is not found!");
+            }
+
+            aim.Title = dto.Title;
+            aim.Price = dto.Price;
+            aim.Date = dto.Date;
+            aim.PicturePath = dto.PicturePath;
+            aim.WayOfSaving = dto.WayOfSaving;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Aim is successfully updated!");
+        }
+
+        //delete
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteAim([FromRoute] long id)
+        {
+            var aim = await _context.Aims.FirstOrDefaultAsync(q => q.Id == id);
+
+            if (aim is null)
+            {
+                return NotFound("Aim is not found!");
+            }
+
+            _context.Aims.Remove(aim);
+            await _context.SaveChangesAsync();
+
+            return Ok("Aim is successfully deleted!");
+        }
+
 
 
     }
