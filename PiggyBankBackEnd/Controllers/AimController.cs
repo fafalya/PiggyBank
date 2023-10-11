cf03facd31bb8e5705f88fcb86029598085d8ca7
fafalya <jsankva@gmail.com>
2023-10-11 17:43:35 +0400

    create DTO for AimController

---
+++ PiggyBankBackEnd/Controllers/AimController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PiggyBankBackEnd.Context;
+using PiggyBankBackEnd.DTOs;
+
+namespace PiggyBankBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AimController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AimController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //create
+        [HttpPost]
+        public async Task<IActionResult> CreateAim([FromBody] CreateUpdateAimDTO dto)
+        {
+            return null;
+        }
+
+
+    }
+}
---
+++ PiggyBankBackEnd/DTOs/CreateUpdateAimDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace PiggyBankBackEnd.DTOs
+{
+    public class CreateUpdateAimDTO
+    {
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public DateTime Date { get; set; }
+        public string PicturePath { get; set; }
+        public int WayOfSaving { get; set; }
+    }
+}
