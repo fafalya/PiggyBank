bf5616eb287c8df35152e81de5ead6a7badb8c78
fafalya <jsankva@gmail.com>
2023-11-12 16:22:51 +0400

    still working on Aims and Users

--- piggybankfrontend/src/pages/Aims.js
+++ piggybankfrontend/src/pages/Aims.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 function Aims() {
     return (
-        <p>Страница для отображения хотелок</p>
+        <Fragment>
+            <div className="main-banner">
+                <div className="counter-content">
+                    <ul>
+                        <li>Days<span id="days"></span></li>
+                        <li>Hours<span id="hours"></span></li>
+                        <li>Minutes<span id="minutes"></span></li>
+                        <li>Seconds<span id="seconds"></span></li>
+                    </ul>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="main-content">
+                                <div className="next-show">
+                                    <i className="fa fa-arrow-up"></i>
+                                    <span>Next Show</span>
+                                </div>
+                                <h6>Opening on Thursday, March 31st</h6>
+                                <h2>The Sunny Hill Festival 2022</h2>
+                                <div className="main-white-button">
+                                    <a href="ticket-details.html">Purchase Tickets</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+        </Fragment>
     );
 };
 export default Aims;
--- piggybankfrontend/src/pages/Users.js
+++ piggybankfrontend/src/pages/Users.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 function Users() {
     return (
-        <p>Страница для отображения пользователей или для авторизации. Пока не решила в общем</p>
+        <Fragment>
+            <div className="page-heading-shows-events">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <h2>Our Shows & Events</h2>
+                            <span>Check out upcoming and past shows & events.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     );
 };
 export default Users;
